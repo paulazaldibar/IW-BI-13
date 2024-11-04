6c737472dfa8bde5bf77cf1fac595f7d50f3d9dc
Mei Torres <mei.torres@opendeusto.es>
2024-11-04 14:21:06 +0100

    plantilla (falta css)

--- appPromoConcert/views.py
+++ appPromoConcert/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 #aqui hay que "diseñar" las páginas
 #y luego en urls.py hay que añadirle dirección a cada pagina
+def index(request):
+    return render(request, 'appPromoConcert/index.html')
