9e9a663cc4b7b512495ec7893a7877ae1e07ee34
Mei Torres <mei.torres@opendeusto.es>
2024-11-13 12:59:37 +0100

    paginas

--- appPromoConcert/urls.py
+++ appPromoConcert/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -10,4 +12,4 @@ urlpatterns = [
     path('single_blog/',views.single_blog, name='single-blog'),
     path('track/',views.track, name='track'),
     path('main/',views.main, name='main'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- appPromoConcert/views.py
+++ appPromoConcert/views.py
@@ -20,3 +20,28 @@ def single_blog(request):
     return render(request, 'appPromoConcert/single-blog.html')
 def track(request):
     return render(request, 'appPromoConcert/track.html')
+
+def elements(request):
+    # Definir las rutas de las imágenes
+    image1 = 'img/elements/g1.jpg'
+    image2 = 'img/elements/g2.jpg'
+    image3 = 'img/elements/g3.jpg'
+    image4 = 'img/elements/g4.jpg'
+    image5 = 'img/elements/g5.jpg'
+    image6 = 'img/elements/g6.jpg'
+    image7 = 'img/elements/g7.jpg'
+    image8 = 'img/elements/g8.jpg'
+
+    # Pasarlas al contexto
+    context = {
+        'image1': image1,
+        'image2': image2,
+        'image3': image3,
+        'image4': image4,
+        'image5': image5,
+        'image6': image6,
+        'image7': image7,
+        'image8': image8,
+    }
+
+    return render(request, 'appPromoConcert/elements.html', context)
