0c0915c8f131c8e4a2681c49ab524ca1022f6395
Mei Torres <mei.torres@opendeusto.es>
2024-11-13 09:54:46 +0100

    añadir urls

--- appPromoConcert/urls.py
+++ appPromoConcert/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('blog/',views.blog, name='blog'),
+    path('contact/',views.contact, name='contact'),
+    path('elements/',views.elements, name='elements'),
+    path('single_blog/',views.single_blog, name='single-blog'),
+    path('track/',views.track, name='track'),
+    path('main/',views.main, name='main'),
 ]
--- appPromoConcert/views.py
+++ appPromoConcert/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render
 #y luego en urls.py hay que añadirle dirección a cada pagina
 def index(request):
     return render(request, 'appPromoConcert/index.html')
+
+def about(request):
+    return render(request, 'appPromoConcert/about.html')
+def blog(request):
+    return render(request, 'appPromoConcert/blog.html')
+def contact(request):
+    return render(request, 'appPromoConcert/contact.html')
+def elements(request):
+    return render(request, 'appPromoConcert/elements.html')
+def main(request):
+    return render(request, 'appPromoConcert/main.html')
+def single_blog(request):
+    return render(request, 'appPromoConcert/single-blog.html')
+def track(request):
+    return render(request, 'appPromoConcert/track.html')
