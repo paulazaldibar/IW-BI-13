be723ceb90ec0c11adba829dfe8ebf276ad101bd
Mei Torres <mei.torres@opendeusto.es>
2024-11-15 00:49:25 +0100

    base de datos

--- appPromoConcert/admin.py
+++ appPromoConcert/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import PromotorMusical, Festival, Interprete
 
 # Register your models here.
+
+admin.site.register(PromotorMusical)
+admin.site.register(Festival)
+admin.site.register(Interprete)
--- appPromoConcert/models.py
+++ appPromoConcert/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class PromotorMusical(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    pais_origen = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class Festival(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    ubicacion = models.CharField(max_length=200)                            #promotor.festivales.all()
+    promotor = models.ForeignKey(PromotorMusical, on_delete=models.CASCADE, related_name='festivales')    
+
+    def __str__(self):
+        return self.nombre
+    
+class Interprete(models.Model):
+    nombre = models.CharField(max_length=100)
+    genero = models.CharField(max_length=50)
+    pais_origen = models.CharField(max_length=100)   #festival.interpretes.all()
+    festivales = models.ManyToManyField(Festival, related_name='interpretes')
+
+    def __str__(self):
+        return self.nombre
