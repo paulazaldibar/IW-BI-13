e945223abf6cc2099b0cf14b18f0ceb654dd5542
Mei Torres <mei.torres@opendeusto.es>
2024-12-01 16:07:00 +0100

    postgre

--- empresaPromoConcert/settings.py
+++ empresaPromoConcert/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +81,9 @@ WSGI_APPLICATION = 'empresaPromoConcert.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
     }
 }
 
