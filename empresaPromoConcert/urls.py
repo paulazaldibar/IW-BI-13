aeb747d2720fcadc9fc0acf93a4d719249ddedef
naroa <n.manterola@opendeusto.es>
2024-12-04 13:35:31 +0100

    multilingüismo

--- empresaPromoConcert/urls.py
+++ empresaPromoConcert/urls.py
@@ -23,10 +23,11 @@ from django.views.i18n import set_language
 urlpatterns = [
     path('admin/', admin.site.urls),
    # path('',include('appPromoConcert.urls')),
+    path('set_language/', set_language, name='set_language'),
 
 ]
 
 urlpatterns += i18n_patterns(
     path('',include('appPromoConcert.urls')),
-    path('set_language/', set_language, name='set_language'),
+   # path('set_language/', set_language, name='set_language'),
 )
